9b8a361c714e2d4970033abe7b10fd2f8cf1de6d
Ruslan Iusupov <ruslanyussupov@gmail.com>
2018-02-08 11:45:30 +0300

    Done

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,34 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+
+public class MasterListFragment extends Fragment {
+
+    public MasterListFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // Get adapter for GridView
+        MasterListAdapter adapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+
+        View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.images_assets_gv);
+
+        gridView.setAdapter(adapter);
+
+        return rootView;
+    }
+}
